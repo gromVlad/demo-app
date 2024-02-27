a95492dc83ec86652d85f3c42fd7f12032c919e9
Ваше ИмяgromVladgit commit -m new <client7289@gmail.com>
2024-02-27 18:47:07 +0300

    fx

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,13 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { WeatherService } from '../features/weather.service';
 
 @Component({
   selector: 'app-demo',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './demo.component.html',
-  styleUrl: './demo.component.scss'
+  styleUrl: './demo.component.scss',
 })
 export class DemoComponent {
+  weather: any;
 
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.weatherService.getWeather('4368').subscribe((data) => {
+      this.weather = data;
+    });
+  }
 }
